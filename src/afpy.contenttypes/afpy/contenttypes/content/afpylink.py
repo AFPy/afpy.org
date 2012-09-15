c9fb39741e8255142029a0c35cade6926558fa1d
sabrina <devnull@localhost>
2012-09-15 11:09:40 +0200

    some code cleaned

--- src/afpy.contenttypes/afpy/contenttypes/content/afpylink.py
+++ src/afpy.contenttypes/afpy/contenttypes/content/afpylink.py
@@ -7,8 +7,6 @@ from Products.Archetypes import atapi
 from Products.ATContentTypes.content import base
 from Products.ATContentTypes.content import schemata
 
-# -*- Message Factory Imported Here -*-
-
 from afpy.contenttypes.interfaces import IAFPYLink
 from afpy.contenttypes.config import PROJECTNAME
 from afpy.contenttypes.content.schemata import AFPYLinkSchema
